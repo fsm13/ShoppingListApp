24b064f0b4cd73eb9aae9f905bee275e71274dc7
fsm13 <fomichevsm@gmail.com>
2023-03-24 16:22:10 +0700

    Creating MainDB class and interface Dao

--- app/src/main/java/fsm/shoppinglistapp/activities/MainActivity.kt
+++ app/src/main/java/fsm/shoppinglistapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package fsm.shoppinglistapp
+package fsm.shoppinglistapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fsm.shoppinglistapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
